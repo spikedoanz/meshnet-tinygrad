3aec3d0eaedf8de584f28adc9c9bb28185d39720
Mike Doan <manhdoanmainmail@gmail.com>
2024-04-26 15:43:34 -0400

    added weight gen script

---
+++ gen_weights.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# Define the shape of the input and the conv_kwargs
+input_shape = (1, 1, 256, 256, 256)
+conv_kwargs = {
+    "in_channels": 1,
+    "out_channels": 21,
+    "kernel_size": 3,
+    "padding": 1,
+    "stride": 1,
+    "dilation": 1,
+}
+
+# Generate fake weights for the convolutional layer
+conv_weights_shape = (
+    conv_kwargs["out_channels"],
+    conv_kwargs["in_channels"],
+    conv_kwargs["kernel_size"],
+    conv_kwargs["kernel_size"],
+    conv_kwargs["kernel_size"]
+)
+conv_weights = np.random.randn(*conv_weights_shape)
+
+# Generate fake weights and biases for the batch normalization layer
+bn_weights_shape = (conv_kwargs["out_channels"],)
+bn_weights = np.random.randn(*bn_weights_shape)
+bn_bias = np.random.randn(*bn_weights_shape)
+
+# Save the generated weights as .npy files
+np.save("conv_weights.npy", conv_weights)
+np.save("bn_weights.npy", bn_weights)
+np.save("bn_bias.npy", bn_bias)
+
+print("Fake weights generated and saved as .npy files.")
